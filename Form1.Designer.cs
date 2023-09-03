77db089e6c954e640dddf729a0c1b730a3c998ac
sandun02 <113725013+sandun02@users.noreply.github.com>
2023-09-04 01:18:35 +0530

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿
+namespace Assignment02._2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtTest = new System.Windows.Forms.TextBox();
+            this.btnBMI = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Yes = new System.Windows.Forms.RadioButton();
+            this.No = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Maroon;
+            this.label1.Location = new System.Drawing.Point(199, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(237, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = " Diagnosis App";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(62, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Weight(Kg):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(62, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 28);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Height(m):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(62, 331);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 28);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Age :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(62, 409);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(270, 28);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Diagnosed with PreDiabetes";
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWeight.Location = new System.Drawing.Point(396, 125);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(179, 34);
+            this.txtWeight.TabIndex = 5;
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHeight.Location = new System.Drawing.Point(396, 202);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(179, 34);
+            this.txtHeight.TabIndex = 6;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAge.Location = new System.Drawing.Point(396, 331);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(179, 34);
+            this.txtAge.TabIndex = 7;
+            // 
+            // txtTest
+            // 
+            this.txtTest.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTest.Location = new System.Drawing.Point(67, 548);
+            this.txtTest.Name = "txtTest";
+            this.txtTest.Size = new System.Drawing.Size(539, 34);
+            this.txtTest.TabIndex = 8;
+            // 
+            // btnBMI
+            // 
+            this.btnBMI.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnBMI.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBMI.Location = new System.Drawing.Point(223, 260);
+            this.btnBMI.Name = "btnBMI";
+            this.btnBMI.Size = new System.Drawing.Size(189, 40);
+            this.btnBMI.TabIndex = 9;
+            this.btnBMI.Text = "Show my BMI";
+            this.btnBMI.UseVisualStyleBackColor = false;
+            this.btnBMI.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.btnTest.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTest.Location = new System.Drawing.Point(135, 482);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(460, 40);
+            this.btnTest.TabIndex = 10;
+            this.btnTest.Text = "Do I Need to have a Blood Screen Test??";
+            this.btnTest.UseVisualStyleBackColor = false;
+            this.btnTest.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(411, 614);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 40);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Yes
+            // 
+            this.Yes.AutoSize = true;
+            this.Yes.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Yes.Location = new System.Drawing.Point(411, 405);
+            this.Yes.Name = "Yes";
+            this.Yes.Size = new System.Drawing.Size(64, 32);
+            this.Yes.TabIndex = 12;
+            this.Yes.TabStop = true;
+            this.Yes.Text = "Yes";
+            this.Yes.UseVisualStyleBackColor = true;
+            // 
+            // No
+            // 
+            this.No.AutoSize = true;
+            this.No.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.No.Location = new System.Drawing.Point(515, 405);
+            this.No.Name = "No";
+            this.No.Size = new System.Drawing.Size(60, 32);
+            this.No.TabIndex = 13;
+            this.No.TabStop = true;
+            this.No.Text = "No";
+            this.No.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(135, 482);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(460, 40);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Do I Need to have a Blood Screen Test??";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(669, 680);
+            this.Controls.Add(this.No);
+            this.Controls.Add(this.Yes);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnBMI);
+            this.Controls.Add(this.txtTest);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Diabetes_Diagnosis";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtTest;
+        private System.Windows.Forms.Button btnBMI;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RadioButton Yes;
+        private System.Windows.Forms.RadioButton No;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment02._2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            double weight = double.Parse(txtWeight.Text);
+            double height = double.Parse(txtHeight.Text);
+
+            double BMI = weight / (height * height);
+
+            MessageBox.Show("Your BMI Value is: "+ BMI);
+            
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double weight = double.Parse(txtWeight.Text);
+                double height = double.Parse(txtHeight.Text);
+
+                double bmi = weight / (height * height);
+
+                bool hasDiabates = Yes.Checked;
+                int age = int.Parse(txtAge.Text);
+
+                if(bmi > 25)
+                {
+                    txtTest.Text = "Take an initial Blood screen test immediately";
+                }
+                else if (hasDiabates)
+                {
+                    txtTest.Text = "Take a Blood screen test per every year";
+                }
+                else if(age > 25)
+                {
+                    txtTest.Text = "Take an initial Blood screen test per every three years";
+                }
+                else
+                {
+                    txtTest.Text = "You do not need to have a Blood screen test";
+                }
+
+            }
+            catch(FormatException e1)
+            {
+                MessageBox.Show(e1.ToString(), "Enter a Numarical Value to the Age");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtWeight.Clear();
+            txtHeight.Clear();
+            txtAge.Clear();
+
+            Yes.Checked = false;
+            No.Checked = false;
+
+            txtTest.Clear();
+        }
+    }
+}
